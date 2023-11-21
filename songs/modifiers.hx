0f3b22d5d3c831d1479c58e25ac326ed2a65d49b
APurples <123270096+APurples@users.noreply.github.com>
2023-11-21 13:03:16 -0600

    reworked poison system

--- songs/modifiers.hx
+++ songs/modifiers.hx
@@ -10,6 +10,9 @@ var botplayTxt:FlxText;
 
 var startDrain:Bool == false;
 var startPoison:Bool == false;
+var stopPoison:Bool == false;
+
+var poisonAmount:Float = 0.75;
 
 function create(){
     botplayTxt = new FlxText(400, 83, FlxG.width - 800, "Botplay", 40);
@@ -29,14 +32,6 @@ function update(elapsed){
     if (FlxG.save.data.earthquake){
         camGame.shake(0.0025 * FlxG.save.data.earthquakeMult, inst.length, null, true);
         camHUD.shake(0.0025 * FlxG.save.data.earthquakeMult, inst.length, null, true);
-    }
-
-    if (startPoison){
-        if (!PlayState.opponentMode){
-            {health -= 0.00025 * FlxG.save.data.poisonMult;}
-        }else{
-            {health += 0.00025 * FlxG.save.data.poisonMult;}
-        }
     }
 
     if (FlxG.save.data.midsongPlaybackRate){
@@ -71,10 +66,19 @@ function postUpdate(){
     if (startDrain){
         if (FlxG.save.data.healthDrain){
             if (!PlayState.opponentMode){
-                if (health > 0.1) {health -= 0.000125 * FlxG.save.data.healthDrainMult;}
+                if (health > 0.1) {health -= 0.000125 + FlxG.save.data.healthDrainMult;}
             }else{
-                if (health > 0.1) {health += 0.000125 * FlxG.save.data.healthDrainMult;}
+                if (health > 0.1) {health += 0.000125 + FlxG.save.data.healthDrainMult;}
             }
+        }
+    }
+
+    if (startPoison){
+        poisonAmount + 0.25;
+        if (!PlayState.opponentMode){
+            {health -= 0.00025 * poisonAmount;}
+        }else{
+            {health += 0.00025 * poisonAmount;}
         }
     }
 }
@@ -82,6 +86,7 @@ function postUpdate(){
 function onPlayerMiss(){
     if (FlxG.save.data.poison){
         startPoison = true;
+        poisonAmount += 0.25;
         if (!PlayState.opponentMode){
             healthBar.createFilledBar(0xFFFF0000, 0xFF8B33FF);
             iconP1.color = 0xFF8B33FF;
@@ -90,25 +95,12 @@ function onPlayerMiss(){
             healthBar.createFilledBar(0xFF8B33FF, 0xFFFF0000);
             iconP2.color = 0xFF8B33FF;
             dad.color = 0xFF8B33FF;
-        }    
-
-        new FlxTimer().start(FlxG.save.data.poisonTimer, function(tmr:FlxTimer){
-            startPoison = false;
-            if (!PlayState.opponentMode){
-                healthBar.createFilledBar(0xFFFF0000, 0xFF66FF33);
-                FlxTween.color(iconP1, 1, 0xFF8B33FF, 0xFFFFFFFF);
-                FlxTween.color(boyfriend, 1, 0xFF8B33FF, 0xFFFFFFFF);
-            }else{
-                healthBar.createFilledBar(0xFF66FF33, 0xFFFF0000);
-                FlxTween.color(iconP2, 1, 0xFF8B33FF, 0xFFFFFFFF);
-                FlxTween.color(dad, 1, 0xFF8B33FF, 0xFFFFFFFF);
-            }    
-        });
+        }
     }
 }
 
 function onSongStart(){
     startDrain = true;
-    FlxTween.tween(botplayTxt, {y: 115}, 1, {ease: FlxEase.sineInOut, type: 4});
+    FlxTween.tween(botplayTxt, {y: 115}, 1, {ease: FlxEase.sineInOut, type: 4}); // type 4 means looping
     FlxTween.tween(botplayTxt, {alpha: 1}, 1);
 }
